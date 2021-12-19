cabc357456eb19503a4e17c0c5ccc1120c94fea1
Zachary Baldwin <zacharybaldwin904@gmail.com>
2021-12-19 17:06:57 -0600

    Add custom commands to Bungee, Release Build

--- src/main/java/me/refrac/simplestaffchat/bungee/utilities/files/Config.java
+++ src/main/java/me/refrac/simplestaffchat/bungee/utilities/files/Config.java
@@ -1,6 +1,23 @@
 /*
- * Copyright (c) Refrac
- * If you have any questions please join my discord https://discord.gg/jVnmm7QnQU
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 RefracDevelopment
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package me.refrac.simplestaffchat.bungee.utilities.files;
 
@@ -22,6 +39,17 @@ public class Config {
     public static String TOGGLE_ON;
     public static String TOGGLE_OFF;
 
+    // Commands
+    public static boolean STAFFCHAT_ENABLED;
+    public static String STAFFCHAT_COMMAND;
+    public static String STAFFCHAT_ALIAS;
+    public static boolean TOGGLE_ENABLED;
+    public static String TOGGLE_COMMAND;
+    public static String TOGGLE_ALIAS;
+    public static boolean RELOAD_ENABLED;
+    public static String RELOAD_COMMAND;
+    public static String RELOAD_ALIAS;
+
     public static void loadConfig() {
         // General
         STAFFCHAT_SYMBOL = Files.getConfig().getString("staffchat-symbol");
@@ -39,5 +67,16 @@ public class Config {
         RELOAD = Files.getConfig().getString("messages.reload");
         TOGGLE_ON = Files.getConfig().getString("messages.toggle-on");
         TOGGLE_OFF = Files.getConfig().getString("messages.toggle-off");
+
+        // Commands
+        STAFFCHAT_ENABLED = Files.getConfig().getBoolean("commands.staffchat.enabled");
+        STAFFCHAT_COMMAND = Files.getConfig().getString("commands.staffchat.command");
+        STAFFCHAT_ALIAS = Files.getConfig().getString("commands.staffchat.alias");
+        TOGGLE_ENABLED = Files.getConfig().getBoolean("commands.toggle.enabled");
+        TOGGLE_COMMAND = Files.getConfig().getString("commands.toggle.command");
+        TOGGLE_ALIAS = Files.getConfig().getString("commands.toggle.alias");
+        RELOAD_ENABLED = Files.getConfig().getBoolean("commands.reload.enabled");
+        RELOAD_COMMAND = Files.getConfig().getString("commands.reload.command");
+        RELOAD_ALIAS = Files.getConfig().getString("commands.reload.alias");
     }
 }
--- src/main/java/me/refrac/simplestaffchat/shared/Settings.java
+++ src/main/java/me/refrac/simplestaffchat/shared/Settings.java
@@ -1,6 +1,23 @@
 /*
- * Copyright (c) Refrac
- * If you have any questions please join my discord https://discord.gg/jVnmm7QnQU
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 RefracDevelopment
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package me.refrac.simplestaffchat.shared;
 
@@ -9,6 +26,6 @@ public class Settings {
     public static final String getName = "SimpleStaffChat2";
     public static final String getDeveloper = "Refrac";
     public static final String getDevUUID = "d9c670ed-d7d5-45fb-a144-8b8be86c4a2d";
-    public static final String getVersion = "2.0.2";
+    public static final String getVersion = "2.0.3";
 
 }
