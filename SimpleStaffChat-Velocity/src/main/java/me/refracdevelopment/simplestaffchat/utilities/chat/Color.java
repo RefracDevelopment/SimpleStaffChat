e89fc6c1ddcf985d2673e3deda72757b585a5d7a
Zach <zacharybaldwin904@gmail.com>
2024-04-29 13:49:47 -0500

    Change java version back to 1.8 for Paper

--- SimpleStaffChat-Velocity/src/main/java/me/refracdevelopment/simplestaffchat/utilities/chat/Color.java
+++ SimpleStaffChat-Velocity/src/main/java/me/refracdevelopment/simplestaffchat/utilities/chat/Color.java
@@ -16,29 +16,6 @@ public class Color {
     }
 
     public Component translate(String message) {
-        message = message
-                .replaceAll("&1", "<dark_blue>")
-                .replaceAll("&2", "<dark_green>")
-                .replaceAll("&3", "<dark_aqua>")
-                .replaceAll("&4", "<dark_red>")
-                .replaceAll("&5", "<dark_purple>")
-                .replaceAll("&6", "<gold>")
-                .replaceAll("&7", "<gray>")
-                .replaceAll("&8", "<dark_gray>")
-                .replaceAll("&9", "<blue>")
-                .replaceAll("&a", "<green>")
-                .replaceAll("&b", "<aqua>")
-                .replaceAll("&c", "<red>")
-                .replaceAll("&d", "<light_purple>")
-                .replaceAll("&e", "<yellow>")
-                .replaceAll("&f", "<white>")
-                .replaceAll("&l", "<bold>")
-                .replaceAll("&k", "<obfuscated>")
-                .replaceAll("&m", "<strikethrough>")
-                .replaceAll("&n", "<underline>")
-                .replaceAll("&o", "<italic>")
-                .replaceAll("&r", "<reset>");
-
         return MiniMessage.miniMessage().deserialize(message);
     }
 
